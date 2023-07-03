80916b797133db8225332ff6b4ee200448573775
fukugansai <fukugansai@gmail.com>
2023-07-04 07:42:36 +0900

    色つけましてん。黒体輻射でね。どんなに見えるかはinstagramであれしてみるといいと思う

--- IFSwithMemory3d/ContentView.swift
+++ IFSwithMemory3d/ContentView.swift
@@ -19,40 +19,45 @@ struct ContentView: View {
             VStack {
                 RealityView { content in
                     // Add the initial RealityKit content
-                    let s = 32
                     let siz:Float = 0.002
-                    
-                    var m:[[Int]] = []
-                    for y in 0 ..< 8 {
-                        m.append([])
-                        for _ in 0 ..< 8 {
-                            let rr = Float.random(in: 0 ..< Float(1))
-                            if rr > 0.4 {
-                                m[y].append(0)
-                            }
-                            else {
-                                m[y].append(1)
-                            }
+
+                    // ねっ、行列の初期化
+                    var m = [[Int]](repeating: [Int](repeating: 0, count: 8), count: 8)
+
+                    // ねっ、その行列のいち部分を１以上の値に設定しなおす。n個までええ！
+                    let maxCountUp = 36
+                    var countUp = 0
+                    while countUp < maxCountUp {
+                        let xi: Int = Int.random(in: 0..<8)
+                        let yi: Int = Int.random(in: 0..<8)
+                        if m[yi][xi] == 0 {
+                            countUp += 1
+                            m[yi][xi] = countUp
                         }
                     }
-                    var v:[[Int]] = [[1],[2],[3],[4],[5],[6],[7],[8]]
-                    for _ in 0 ..< 6 {
-                        var nv:[[Int]] = []
+
+                    // それでねっ、ありえるすべての変換列をね、するのよね
+                    var v:[(h:Int, ar:[Int])] = [(1,[1]),(1,[2]),(1,[3]),(1,[4]),(1,[5]),(1,[6]),(1,[7]),(1,[8])]
+                    let maxDepth = 6
+                    for _ in 0 ..< maxDepth {
+                        var nv:[(h:Int, ar:[Int])] = []
                         for vi in v {
-                            let vli = vi.last!-1
+                            let vli = vi.ar.last!-1
                             //print(vli)
                             for vn in 0 ..< 8 {
-                                if m[vli][vn] == 1 {
-                                    var vin:[Int] = vi
-                                    vin.append(vn+1)
+                                if m[vli][vn] >= 1 {
+                                    var vin:(h:Int, ar:[Int]) = vi
+                                    vin.ar.append(vn+1)
+                                    if m[vli][vn] > vin.h {
+                                        vin.h = m[vli][vn]
+                                    }
                                     nv.append(vin)
                                 }
                             }
                         }
                         v = nv
                     }
-                    print(m)
-                    
+ 
                     let modelEntityMesh = MeshResource.generateBox(size: SIMD3<Float>(siz, siz, siz))
                     let meshModels = modelEntityMesh.contents.models
 
@@ -62,55 +67,28 @@ struct ContentView: View {
                         m1 = mm
                     }
 
-                    var transformedContents = MeshResource.Contents()
-                    transformedContents.instances.removeAll()
+                    var transformedContents:[MeshResource.Contents] = []
+                    for _ in 0..<maxDepth+2 {
+                        var tc = MeshResource.Contents()
+                        tc.instances.removeAll()
+                        transformedContents.append(tc)
+                    }
                     let step:Float = siz * 32.0
                     var count:Int = 0
                     for vi in v {
                         var fx:Float = 0.0
                         var fy:Float = 0.0
                         var fz:Float = 0.0
-                        for vn in vi {
+                        var cc = 0
+                        for vn in vi.ar {
                             fx /= 2.0
                             fy /= 2.0
                             fz /= 2.0
-                            switch vn {
-                            case 1:
-                                fx -= step
-                                fy -= step
-                                fz -= step
-                            case 2:
-                                fx += step
-                                fy -= step
-                                fz -= step
-                            case 3:
-                                fx -= step
-                                fy += step
-                                fz -= step
-                            case 4:
-                                fx += step
-                                fy += step
-                                fz -= step
-                            case 5:
-                                fx -= step
-                                fy -= step
-                                fz += step
-                            case 6:
-                                fx += step
-                                fy -= step
-                                fz += step
-                            case 7:
-                                fx -= step
-                                fy += step
-                                fz += step
-                            case 8:
-                                fx += step
-                                fy += step
-                                fz += step
-                            default:
-                                fx -= step
-                                fy -= step
-                                fz -= step
+                            fx = [1, 3, 5, 7].contains(vn) ? fx - step : fx + step
+                            fy = [1, 2, 5, 6].contains(vn) ? fy - step : fy + step
+                            fz = [1, 2, 3, 4].contains(vn) ? fz - step : fz + step
+                            if [1, 2, 5, 6].contains(vn) {
+                                cc += 1
                             }
                         }
                         let f44 = simd_float4x4(
@@ -120,14 +98,21 @@ struct ContentView: View {
                                       simd_float4(fx, fy, fz, 1.0)))
                         count += 1
                         let mri = MeshResource.Instance(id: String(count), model: m1!.id, at: f44)
-                        transformedContents.instances.insert(mri)
+                        precondition(0 <= cc && cc <= maxDepth+1)
+                        transformedContents[cc].instances.insert(mri)
                     }
 
-                    transformedContents.models = meshModels
-                    let transformedMesh = try! MeshResource.generate(from: transformedContents)
-                    let modelb = ModelEntity(mesh: transformedMesh, materials: [SimpleMaterial(color: UIColor(red:0.5, green:0.5, blue: 1, alpha:1), isMetallic: true)])
-                    
-                    content.add(modelb)
+                    var tcc = 0.0
+                    count = 0
+                    for var tc in transformedContents {
+                        tcc = Double(count) / Double(maxDepth)
+                        //tcc = tcc * tcc
+                        tc.models = meshModels
+                        let transformedMesh = try! MeshResource.generate(from: tc)
+                        let modelb = ModelEntity(mesh: transformedMesh, materials: [SimpleMaterial(color: UIColor(red:1.1-0.9*tcc, green:0.7-1.0*tcc, blue: 0.1, alpha:1.0), isMetallic: false)])
+                        content.add(modelb)
+                        count+=1
+                    }
                 } update: { content in
                 }
                 .rotation3DEffect(
